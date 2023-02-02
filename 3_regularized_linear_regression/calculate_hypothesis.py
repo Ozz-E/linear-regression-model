8afbbd6b8527a7c2e458f476b8733c708ca760c5
Ozz-E <67159787+Ozz-E@users.noreply.github.com>
2023-02-02 12:58:26 +0000

    .gitignore is now working

---
+++ 3_regularized_linear_regression/calculate_hypothesis.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def calculate_hypothesis(X, theta, i):
+    """
+        :param X            : 2D array of our dataset
+        :param theta        : 1D array of the trainable parameters
+        :param i            : scalar, index of current training sample's row
+    """
+
+    hypothesis = 0
+    for k in range(theta.shape[0]):
+        hypothesis += X[i, k] * theta[k]
+
+    return hypothesis
