0998d28be2dcae1fe1cacc72843e53cb569f6d08
Ozz-E <67159787+Ozz-E@users.noreply.github.com>
2023-02-22 16:28:58 +0000

    added explanations

--- 1_one_variable/hypothesis_to_vector.py
+++ 1_one_variable/hypothesis_to_vector.py
@@ -1,6 +1,11 @@
 from calculate_hypothesis import *
 
 def hypothesis_to_vector(X, theta):
+
+    ''' This function takes in a matrix X and a vector theta and returns a vector 
+        of the same size as X, where each element is the value 
+        of the hypothesis function at the corresponding element of X.
+    '''
     
     hypothesis_vec = np.array([], dtype=np.float32)
     for i in range(X.shape[0]):
